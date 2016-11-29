d2396fb06e79ab1d47f0737688bca9785a00a956
Fuping <fupingma@me.com>
2016-11-29 22:58:19 +1100

    Failed test cases added

--- src/test/com/codility/demo/EquilibriumIndexTest.java
+++ src/test/com/codility/demo/EquilibriumIndexTest.java
@@ -1,5 +1,6 @@
 package com.codility.demo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,9 +14,50 @@ public class EquilibriumIndexTest {
 
 	int index = EquilibriumIndex.findEquilibriumIndex(A);
 
-	System.out.println("index: " + index);
-
 	assertTrue(index == 1 || index == 3 || index == 7);
     }
 
+    @Test
+    // Sequence with extremely large numbers testing arithmetic overflow
+    public void testExtremelyLargeNumbers() {
+
+	int[] A = { 1082132608, 0, 1082132608 };
+
+	int index = EquilibriumIndex.findEquilibriumIndex(A);
+
+	assertEquals(1, index);
+    }
+
+    @Test
+    // Sequence with extremely negative numbers testing arithmetic overflow
+    public void testExtremelyNegativeNumbers() {
+
+	int[] A = { 0, -1000000000, -1000000000, -1000000000, -1000000000, -294967296 };
+
+	int index = EquilibriumIndex.findEquilibriumIndex(A);
+
+	assertEquals(-1, index);
+    }
+
+    @Test
+    // arithmetic overflow tests
+    public void testOverflow1() {
+
+	int[] A = { 2147483647, 2147483647, 2147483647 };
+
+	int index = EquilibriumIndex.findEquilibriumIndex(A);
+
+	assertEquals(1, index);
+    }
+
+    @Test
+    // arithmetic overflow tests
+    public void testOverflow2() {
+
+	int[] A = { 0, 2147483647, 2147483647, 2147483647 };
+
+	int index = EquilibriumIndex.findEquilibriumIndex(A);
+
+	assertEquals(1, index);
+    }
 }
